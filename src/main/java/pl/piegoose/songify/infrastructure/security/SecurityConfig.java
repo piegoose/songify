0b90ca0e7cda1074db0716011a9316bee637230b
piegoose <jedrzej.sikora@puszkin.eu>
2026-04-21 13:19:42 +0200

    54. admin can only mutate data

--- src/main/java/pl/piegoose/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/pl/piegoose/songify/infrastructure/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.piegoose.songify.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -28,9 +29,29 @@ class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable);
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
-        http.authorizeHttpRequests(authorize ->
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/swagger-resources").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
+                .requestMatchers("users/register/**").permitAll()
+                .requestMatchers(HttpMethod.GET,"/songs/**").permitAll()
+                .requestMatchers(HttpMethod.GET,"/artist/**").permitAll()
+                .requestMatchers(HttpMethod.GET,"/album/**").permitAll()
+                .requestMatchers(HttpMethod.GET,"/genre/**").permitAll()
+                .requestMatchers(HttpMethod.POST,"/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT,"/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PATCH,"/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE,"/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST,"/artist/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT,"/artist/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PATCH,"/artist/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE,"/artist/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST,"/album/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT,"/album/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST,"/genre/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST,"/users/**").hasRole("ADMIN")
 
-                authorize.anyRequest().authenticated());
+                .anyRequest().authenticated());
         return http.build();
     }
 
