8581a2e20e0c0a166e9096b8a12b4a4df7ff44cd
piegoose <jedrzej.sikora@puszkin.eu>
2026-04-29 12:37:56 +0200

    61. cookies +

--- src/main/java/pl/piegoose/songify/infrastructure/security/jwt/JwtConfigurationProperties.java
+++ src/main/java/pl/piegoose/songify/infrastructure/security/jwt/JwtConfigurationProperties.java
@@ -1,10 +1,9 @@
 package pl.piegoose.songify.infrastructure.security.jwt;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(value = "auth.jwt")
-public record JwtConfigurationProperites(
+public record JwtConfigurationProperties(
         String secret,
         long expirationMinutes,
         String issuer
